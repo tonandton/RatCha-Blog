a74b039cfb19adb4aa968f7f3e8ced9eadd8f4bf
tonandton <www.sorasakok@gmail.com>
2025-07-05 22:32:42 +0700

    create blog

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import AuthLink from "../authLink/AuthLink";
+import ThemeToggle from "../themeToggle/ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>RatCha | Blog</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/">Homepage</Link>
+        <Link href="/">Blog</Link>
+        <Link href="/">Work</Link>
+        <Link href="/">About</Link>
+        <AuthLink />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
