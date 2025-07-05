576f1b1a3bd968ed4ca6c8eec61979382b2b1906
tonandton <www.sorasakok@gmail.com>
2025-07-05 22:41:23 +0700

    create blog

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/btosecxnd/Desktop/ratcha-blog/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
