989318a74edf198922573988d1e494e119ae2225
tonandton <www.sorasakok@gmail.com>
2025-09-25 14:05:09 +0700

    update

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
-import { create } from 'domain'
+import Forminput from '@/components/Form/Forminput'
 
 const createProfileAction = async (formData: FormData) => {
      'use server'
@@ -19,9 +17,10 @@ const CreateProfile = () => {
                <h1 className="text-2xl font-semibold mb-8 capitalize">new user </h1>
                <div className="border-none bg-white/30 p-8 rounded max-w-lg">
                     <form action={createProfileAction}>
-                         <div className="mb-2">
-                              <Label htmlFor='firstName'>First Name</Label>
-                              <Input name="firstName" type="text" className="rounded" />
+                         <div className="grid md:grid-cols-2 gap-4 mt-4">
+                              <Forminput name="firstName" label="test" type="typename" placeholder='Phoc' />
+                              <Forminput name="lastName" label="Last name" type="2type" />
+                              <Forminput name="email" label="Email" type="email" />
                          </div>
                          <Button type="submit" size="lg" className="rounded ">Create Profile</Button>
                     </form>
---
+++ components/Form/Forminput.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+type FormInputProps = {
+     name: string,
+     type: string,
+     label?: string,
+     defaultValue?: string,
+     placeholder?: string
+
+}
+
+const Forminput = (props: FormInputProps) => {
+     const { name, type, label, defaultValue, placeholder } = props
+     return (
+          <div className="mb-2">
+               <Label htmlFor={name}>{label}</Label>
+               <Input name={name} type={type} placeholder={placeholder}
+                    defaultValue={defaultValue} className="rounded" />
+          </div>
+     )
+}
+
+export default Forminput
